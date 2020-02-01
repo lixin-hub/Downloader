b77bf1744c691f3b9a0510568f2df6746b97d535
lixin-hub <763819849@qq.com>
2020-02-01 14:49:31 +0800

    continue

---
+++ Download/app/src/main/java/com/example/download/DownloadUtil.java
@@ -0,0 +1,169 @@
+package com.example.download;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
+import android.os.Environment;
+import java.io.RandomAccessFile;
+import java.io.InputStream;
+import java.util.Random;
+import android.widget.TextView;
+import android.content.Context;
+
+public class DownloadUtil
+{	
+
+	MainActivity context;
+	StringBuilder builder = new StringBuilder();;
+	TextView text;
+	long length;
+	long block;
+	int threadNum;
+
+	public DownloadUtil(MainActivity context)
+	{
+		this.context = context;
+	}
+
+    public void download(String path, int threadNum, TextView text) throws MalformedURLException, IOException
+    {
+		this.text = text;
+		this.threadNum = threadNum;
+
+		for (int threadId=0;threadId < threadNum;threadId++)
+		{
+			new DownloadThread(path, threadId).start();
+			builder.append("threadId=" + threadId + "启动" + "\n");
+			text.setText(builder.toString());
+		}
+
+	}
+
+
+
+    private static String getFileName(String path)
+    {
+        return    path.substring(path.lastIndexOf("/")); 
+    }
+
+    private class DownloadThread extends Thread
+    {
+
+        String path;
+        File file;
+        int threadId;
+
+        public DownloadThread(String path, int threadId)
+        {
+
+            this.path = path;
+            this.threadId = threadId;
+
+        }
+
+
+        @Override
+        public void run() 
+        {
+
+
+            try
+            {
+                URL url=new URL(path);
+                try
+                {
+                    HttpsURLConnection conn=(HttpsURLConnection) url.openConnection();
+                    conn.setConnectTimeout(5000);
+                    conn.setRequestMethod("GET");
+					length = getContentLength(path);
+					int block = 0;
+					if (length % threadNum == 0)
+					{block = (int) (length / threadNum);
+					}
+					if (length % threadNum != 0)
+					{
+						block = (int) length / threadNum + 1;
+
+					}
+
+					int start=threadId * block;
+					int end=(threadId + 1) * block;
+					builder.append("threadId=" + threadId + "\n" + "block=" + block + "start=" + start + "end=" + end + "\n");
+					context.runOnUiThread(new Runnable(){
+
+							@Override
+							public void run()
+							{
+								text.setText(builder.toString());
+							}
+						});
+
+
+					conn.setRequestProperty("Range", "bytes=" + start + "-" + end);
+					String directory=Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath();
+					file = new File(directory + getFileName(path));
+					if (file.exists())
+					{file.delete();}
+					else
+					{file.createNewFile();}
+					RandomAccessFile randAccessFile=new RandomAccessFile(file, "rw"); 
+					randAccessFile.setLength(length);
+                    randAccessFile.seek(start);
+                    InputStream is=conn.getInputStream();
+                    byte[] b=new byte[1024];
+                    int len=0;
+                    while ((len = is.read(b)) != -1)
+                    {
+                        randAccessFile.write(b, 0, len);
+                    }
+                    randAccessFile.close();
+                    is.close();
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+					builder.append(e.toString());
+					text.setText(builder.toString());
+                }
+
+            }
+            catch (MalformedURLException e)
+            {
+                e.printStackTrace();
+				builder.append(e.toString());
+				text.setText(builder.toString());
+            }
+
+            super.run();
+        }
+
+
+		public long getContentLength(String path)
+		{
+			try
+			{
+				URL url=new URL(path);
+				HttpsURLConnection conn=(HttpsURLConnection) url.openConnection();
+				conn.setConnectTimeout(5000);
+				conn.setRequestMethod("GET");
+				int length=conn.getContentLength();
+				if (length != 0)
+				{
+					return length;
+				}
+			}
+			catch (Exception e)
+			{
+
+				e.printStackTrace();
+			}
+			return 0;
+		}
+
+    }
+
+
+
+
+}
--- Download/app/src/main/java/com/example/download/MainActivity.java
+++ Download/app/src/main/java/com/example/download/MainActivity.java
@@ -3,16 +3,45 @@ package com.example.download;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+	TextView  text;
+	EditText edit;
+	Button bt;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-		
 		Toolbar toolbar=(Toolbar)findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
+		text=findViewById(R.id.activity_mainTextView);
+		edit=findViewById(R.id.activity_mainEditText);
+		bt=findViewById(R.id.activity_mainButton);
+		bt.setOnClickListener(new View.OnClickListener(){
+
+				@Override
+				public void onClick(View p1)
+				{
+					String path= edit.getText().toString();
+					try
+					{
+				new DownloadUtil(MainActivity.this).download(path, 3,text);
+				
+					}
+					catch (IOException e)
+					{
+						e.printStackTrace();
+					}
+				}
+			});
+		
 
     }
 
